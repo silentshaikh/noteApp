f33441988e46d3e84efd7ec7e79d32a3f280e19e
silentshaikh <sheikhabdulmoiz283@gmail.com>
2023-08-07 00:07:12 +0500

    update files

--- app.js
+++ app.js
@@ -7,30 +7,34 @@ navBar.style.color = "white";
 myContaier.style.backgroundColor = "#0fbcf9";
 myContaier.style.border = "1px solid #fff";
 myContaier.style.color = "#fff";
+myContaier.style.boxShadow = "15px 15px 15px #0fbcf9";
 head.style.color = "#48dbfb";
 myContaier.style.fontFamily = `'Poppins', sans-serif`;
 head.style.fontFamily = `'Poppins', sans-serif`;
 addBtn.style.backgroundColor = "#4dddad";
-addBtn.style.border="1px solid white" ;
+addBtn.style.border = "1px solid white";
 addBtn.style.color = "#fff";
+addBtn.style.padding = "8px";
+addBtn.style.borderRadius = "5px";
+
 showText();
-addBtn.addEventListener("click",() => {
+addBtn.addEventListener("click", () => {
     let titleNotes = document.getElementById("title");
-    let inptNotes = document.getElementById("createnotes"); 
+    let inptNotes = document.getElementById("createnotes");
     let getNotes = localStorage.getItem("Notes");
-    if(inptNotes.value === "" || titleNotes.value == ""){
+    if (inptNotes.value === "" || titleNotes.value == "") {
         alert("Fill Input");
-    }else{
-        if(getNotes === null){
-            objNotes = []; 
-        }else{
+    } else {
+        if (getNotes === null) {
+            objNotes = [];
+        } else {
             objNotes = JSON.parse(getNotes);
         }
         objNotes.push({
             title: titleNotes.value,
             notes: inptNotes.value,
         });
-        localStorage.setItem("Notes",JSON.stringify(objNotes));
+        localStorage.setItem("Notes", JSON.stringify(objNotes));
         inptNotes.value = "";
         titleNotes.value = "";
         console.log(objNotes);
@@ -38,50 +42,65 @@ addBtn.addEventListener("click",() => {
     }
 });
 
- function showText(){
+function showText() {
     let getNotes = localStorage.getItem("Notes");
-    if(getNotes === null){
+    if (getNotes === null) {
         objNotes = [];
-    }else{
+    } else {
         objNotes = JSON.parse(getNotes);
     }
     let textHtml = ``;
-    objNotes.forEach((element,index) => {
-        textHtml += `<div class="notecard  mt-3 mx-4 card" style="width: 18rem;">
+    objNotes.forEach((element, index) => {
+        textHtml += `<div  class="notecard  mt-5 mx-4 card" style="width: 18rem;">
         <div class="card-body">
         <h5 class="card-title"> ${element.title}</h5>
         <p class="card-text" >${element.notes}</p>
-        <button id = "${index}" onclick = "deleteNotes(this.id)" class="btn btn-primary">Delete</button>
+        <button id = "${index}" onclick = "deleteNotes(this.id)" class="btn">Delete</button>
         </div>
         </div>`;
     });
     let para = document.getElementById("textpara");
-    if(objNotes.length !== 0){
+    if (objNotes.length !== 0) {
         para.innerHTML = textHtml;
-    }
+    };
+    let noteBox = document.querySelectorAll(".notecard");
+Array.from(noteBox).forEach(e => {
+    e.style.backgroundColor = "#4dddad";
+    e.style.color = "#fff";
+    e.style.fontFamily = `'Poppins', sans-serif`;
+    e.style.boxShadow = "15px 15px 15px #4dddad";
+    e.style.marginBottom = "20px";
+});
+let btn = document.querySelectorAll(".btn");
+   Array.from(btn).forEach(e => {
+        e.style.backgroundColor = "#0fbcf9";
+        e.style.color = "#fff";
+        e.style.border = "none";
+    });
 };
 
 let deleteNotes = (index) => {
-  let getNotes = localStorage.getItem("Notes");
-  if(getNotes === null){
-    objNotes = [];
-  }else{
-    objNotes = JSON.parse(getNotes);
-  }
-  objNotes.splice(index,1);
-  localStorage.setItem("Notes",JSON.stringify(objNotes));
-  showText();
+    let getNotes = localStorage.getItem("Notes");
+    if (getNotes === null) {
+        objNotes = [];
+    } else {
+        objNotes = JSON.parse(getNotes);
+    }
+    objNotes.splice(index, 1);
+    localStorage.setItem("Notes", JSON.stringify(objNotes));
+    showText();
 };
 let searchBox = document.getElementById("searchbox");
 searchBox.addEventListener("input", () => {
     let inpValue = searchBox.value;
     let cardNotes = document.getElementsByClassName("notecard");
     Array.from(cardNotes).forEach((element) => {
-        let cardPara = element.getElementsByTagName("p")[0].innerText;
-        if(cardPara.includes(inpValue)){
+        let cardPara = element.getElementsByTagName("h5")[0].innerText;
+        if (cardPara.includes(inpValue)) {
             element.style.display = "block";
-        }else{
+        } else {
             element.style.display = "none";
         }
     })
 })
+
